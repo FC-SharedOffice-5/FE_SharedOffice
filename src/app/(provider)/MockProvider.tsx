cc6924ee150ad92dfc8b76dc0c7b2488ffa7d75d
knoa0405 <knoa0405@gmail.com>
2024-05-20 06:17:44 +0900

    refactor: code in mock provider

--- src/app/(provider)/MockProvider.tsx
+++ src/app/(provider)/MockProvider.tsx
@@ -7,27 +7,21 @@ type TProps = Readonly<{
 }>;
 
 export default function MockProvider({ children }: TProps) {
-  const [mockingEnabled, enableMocking] = useState(false);
-  /**
-   * msw 환경 설정
-   */
-  const configMsw = async () => {
-    // next dev를 이용하여 개발 환경에서 동작했을 때만 msw 동작
-    if (process.env.NODE_ENV === 'development') {
-      const { worker } = await import('@/mocks/browser');
-      await worker.start({ onUnhandledRequest: 'bypass' });
-
-      enableMocking(true);
-    }
-  };
+  const [isInit, setIsInit] = useState<boolean>(false);
 
   useEffect(() => {
-    configMsw();
-  }, []);
+    async function configMsw() {
+      const { worker } = await import('@/mocks/browser');
+      console.warn('worker start MSW warning 뜨는 이유 : react strict mode 로 두번 호출됨');
 
-  if (!mockingEnabled) {
-    return null;
-  }
+      await worker.start({ onUnhandledRequest: 'bypass' });
+      setIsInit(true);
+    }
+    // next dev를 이용하여 개발 환경에서 동작했을 때만 msw 동작
+    if (process.env.NODE_ENV === 'development' && !isInit) {
+      configMsw();
+    }
+  }, [isInit]);
 
   return <>{children}</>;
 }
