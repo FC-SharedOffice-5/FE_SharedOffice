7ab050de57233534b053f2c8e0d08da0da56b638
knoa0405 <knoa0405@gmail.com>
2024-05-20 05:33:23 +0900

    chore: delete useless condition in mock provider

--- src/app/(provider)/MockProvider.tsx
+++ src/app/(provider)/MockProvider.tsx
@@ -13,7 +13,7 @@ export default function MockProvider({ children }: TProps) {
    */
   const configMsw = async () => {
     // next dev를 이용하여 개발 환경에서 동작했을 때만 msw 동작
-    if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+    if (process.env.NODE_ENV === 'development') {
       const { worker } = await import('@/mocks/browser');
       await worker.start({ onUnhandledRequest: 'bypass' });
 
