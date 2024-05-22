f2157d9f6206c0066536ee36b8902465fae57d87
knoa0405 <knoa0405@gmail.com>
2024-05-22 10:51:44 +0900

    chore: setup svgr library

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,24 @@ const nextConfig = {
     instrumentationHook: true,
   },
   webpack: (config, { isServer }) => {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
     if (isServer) {
       if (Array.isArray(config.resolve.alias)) {
         config.resolve.alias.push({ name: 'msw/browser', alias: false });
