17f90a3ce8bf73cbe9c49a2a9b42022702718270
knoa0405 <knoa0405@gmail.com>
2024-05-27 11:48:20 +0900

    feat: add select gender modal & format birth in user info page

--- src/app/(auth)/signup/user-info/page.tsx
+++ src/app/(auth)/signup/user-info/page.tsx
@@ -1,40 +1,89 @@
 'use client';
 
 import ChevronDownIcon from '@/assets/icons/ChevronDownIcon';
-import TextInput from '@/components/text-input';
-import { useForm } from 'react-hook-form';
+import PrimaryButton from '@/components/PrimaryButton';
+import Input from '@/components/Input';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useCallback, useState } from 'react';
+import GenderSelectModal from './_components/GenderSelectModal';
+import { useRouter } from 'next/navigation';
+
+export type TFormValues = {
+  name: string;
+  gender: string;
+  birth: string;
+};
 
 export default function UserInfoPage() {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedGender, setSelectedGender] = useState('');
+
   const {
-    register,
-    watch,
-    formState: { errors },
-    setValue,
-  } = useForm({ mode: 'onChange' });
+    formState: { errors, isValid },
+    control,
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = () => {
+    router.push('/signup/email-verification');
+  };
+
+  const handleGenderClick = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleSelectGender = useCallback((gender: string) => {
+    setSelectedGender(gender);
+    setIsModalOpen(false);
+  }, []);
 
   return (
     <main className={`flex h-full flex-col items-center justify-between px-4`}>
-      <form>
-        <TextInput
-          type="text"
-          label="이름"
-          name="email"
-          placeholder="예) mile@mile.co.kr"
-          setValue={setValue}
-          register={register}
-          errors={errors}
-        />
-        <TextInput
-          type="text"
+      <div />
+      <form className="flex w-full flex-col gap-10">
+        <div className="flex gap-2">
+          <Input
+            control={control}
+            validation={{
+              required: true,
+            }}
+            label="이름"
+            name="name"
+            placeholder="홍길동"
+          />
+          <div className="flex w-full flex-col">
+            <label className="label-small">성별</label>
+            <div
+              className="body-small placeholder:body-small flex h-10 w-full cursor-pointer items-center justify-between border-b-[0.75px] border-[#111]/[.4]"
+              onClick={handleGenderClick}
+            >
+              <span>{selectedGender || '성별을 선택하세요.'}</span>
+              <ChevronDownIcon />
+            </div>
+          </div>
+        </div>
+        <Input
+          control={control}
+          validation={{
+            required: true,
+          }}
           label="생년월일"
-          name="email"
+          name="birth"
+          maxLength={8}
           placeholder="1970.01.01"
-          setValue={setValue}
-          register={register}
-          errors={errors}
-          suffix={() => <ChevronDownIcon />}
         />
       </form>
+      <PrimaryButton
+        disabled={!(isValid && selectedGender)}
+        name="다음으로"
+        handleClick={handleSubmit(onSubmit)}
+      />
+      <GenderSelectModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSelect={handleSelectGender}
+      />
     </main>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,111 +1,118 @@
 'use client';
 
 import Image from 'next/image';
-import { ElementType, InputHTMLAttributes, useState } from 'react';
-import { UseFormSetValue, UseFormRegister, FieldValues, FieldErrors } from 'react-hook-form';
+import { ChangeEvent, ElementType, InputHTMLAttributes, useMemo, useState } from 'react';
+import { useController, Control } from 'react-hook-form';
 import clsx from 'clsx';
 import { Field, Input as HeadlessInput, Label } from '@headlessui/react';
+import { formatBirthDate } from '@/utils/formatBirth';
 
 type TInputProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
-  setValue?: UseFormSetValue<FieldValues>;
-  register?: UseFormRegister<FieldValues>;
-  errors?: FieldErrors;
+  control: Control;
   validation?: object;
-  error?: boolean;
   suffix?: ElementType;
 };
 
 const Input = ({
   label,
   name,
-  setValue,
-  register,
-  errors,
+  control,
   validation,
-  error,
   suffix: SuffixComponent,
   ...props
 }: TInputProps) => {
+  const { field, fieldState } = useController({ control, name, rules: validation });
   const { type = 'text', disabled = false } = props;
-  const [inputValue, setInputValue] = useState('');
   const [inputType, setInputType] = useState(type);
-  const isErrorPresent = !!errors?.[name];
+  const isErrorPresent = !!fieldState.error;
 
   const togglePasswordVisibility = () => {
     setInputType((prevType) => (prevType === 'password' ? 'text' : 'password'));
   };
 
+  const isVisibleDeleteValueButton = useMemo(
+    () => !disabled && field.value && field.name !== 'code' && !field.name.includes('password'),
+    [disabled, field.name, field.value],
+  );
+
   return (
-    <Field
-      disabled={disabled}
-      className="relative flex w-full flex-col"
-    >
-      <Label
-        className={clsx(
-          'label-small data-[disabled]:text-[#111]/[.4]',
-          (isErrorPresent || error) && 'text-error',
-        )}
+    <>
+      <Field
+        disabled={disabled}
+        className="relative flex w-full flex-col"
       >
-        {label}
-      </Label>
-      <HeadlessInput
-        {...props}
-        {...register?.(name, validation)}
-        onInput={(e) => {
-          if (type === 'number') {
-            e.currentTarget.value = e.currentTarget.value.slice(0, 6);
-          }
-          setInputValue(e.currentTarget.value);
-        }}
-        className={clsx(
-          'body-small placeholder:body-small h-10 w-full border-b-[0.75px] border-[#111]/[.4] focus:bg-white focus:outline-none data-[focus]:border-[#111] data-[disabled]:bg-white data-[disabled]:text-[#111]/[.4]',
-          (isErrorPresent || error) && 'border-error data-[focus]:border-error',
-        )}
-      />
-      {setValue && inputValue && !disabled && (
-        <button
-          type="button"
-          onClick={() => {
-            setValue(name, '');
-            setInputValue('');
+        <Label
+          className={clsx(
+            'label-small data-[disabled]:text-[#111]/[.4]',
+            isErrorPresent && 'text-error',
+          )}
+        >
+          {label}
+        </Label>
+        <HeadlessInput
+          {...props}
+          onChange={field.onChange}
+          onBlur={field.onBlur}
+          name={field.name}
+          value={field.value ?? ''}
+          onInput={(e: ChangeEvent<HTMLInputElement>) => {
+            if (type === 'number') {
+              if (e.currentTarget.value.length > e.currentTarget.maxLength)
+                e.currentTarget.value = e.currentTarget.value.slice(0, e.currentTarget.maxLength);
+            }
+            if (name === 'birth') {
+              e.currentTarget.value = formatBirthDate(e.currentTarget.value);
+            }
           }}
-          className="absolute bottom-[10px] right-0"
-        >
-          <Image
-            src="/delete.svg"
-            alt="전체 삭제"
-            width={20}
-            height={20}
-            className="hover:cursor-pointer"
-          />
-        </button>
-      )}
-      {type === 'password' && (
-        <button
-          type="button"
-          onClick={togglePasswordVisibility}
-          className="absolute bottom-[10px] right-0"
-        >
-          <Image
-            src={inputType === 'password' ? '/hide.svg' : '/hide-off.svg'}
-            alt="비밀번호 보이기/숨기기"
-            width={20}
-            height={20}
-            className="hover:cursor-pointer"
-          />
-        </button>
-      )}
-      {SuffixComponent && (
-        <div className="absolute bottom-[10px] right-0">
-          <SuffixComponent />
-        </div>
-      )}
+          className={clsx(
+            'body-small placeholder:body-small h-10 w-full border-b-[0.75px] border-[#111]/[.4] focus:bg-white focus:outline-none data-[focus]:border-[#111] data-[disabled]:bg-white data-[disabled]:text-[#111]/[.4]',
+            isErrorPresent && 'border-error data-[focus]:border-error',
+          )}
+        />
+        {isVisibleDeleteValueButton && (
+          <button
+            type="button"
+            onClick={() => {
+              field.onChange('');
+            }}
+            className="absolute bottom-[10px] right-0"
+          >
+            <Image
+              src="/delete.svg"
+              alt="전체 삭제"
+              width={20}
+              height={20}
+              className="hover:cursor-pointer"
+            />
+          </button>
+        )}
+        {type === 'password' && (
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute bottom-[10px] right-0"
+          >
+            <Image
+              src={inputType === 'password' ? '/hide.svg' : '/hide-off.svg'}
+              alt="비밀번호 보이기/숨기기"
+              width={20}
+              height={20}
+              className="hover:cursor-pointer"
+            />
+          </button>
+        )}
+        {SuffixComponent && (
+          <div className="absolute bottom-[10px] right-0">
+            <SuffixComponent />
+          </div>
+        )}
+      </Field>
       {isErrorPresent && (
-        <span className="body-small text-error">{errors?.[name]?.message?.toString()}</span>
+        <span className="body-small text-error">{fieldState?.error?.message?.toString()}</span>
       )}
-    </Field>
+    </>
   );
 };
 
