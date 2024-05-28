bc6a641dd4c52a7b74e88c09712853db8859d4c2
silver0108 <kimsoeun0108@gmail.com>
2024-05-28 17:23:23 +0900

    refactor: use react-hook-form for error handling and button enabling

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -20,7 +20,14 @@ export const signInWithCredentials = async (data: FieldValues, isChecked: boolea
     if (err instanceof AuthError) {
       switch (err.type) {
         case 'CredentialsSignin':
-          return { message: err.cause as unknown as string };
+          const errorCause = err.cause as unknown as string;
+
+          if (errorCause === 'not_email') {
+            return { errorType: 'email', errorMessage: '존재하지 않는 이메일입니다.' };
+          } else if (errorCause === 'not_password') {
+            return { errorType: 'password', errorMessage: '비밀번호가 일치하지 않습니다.' };
+          }
+
         default:
           return { message: '문제가 발생했습니다.' };
       }
