5f70df80ffc9103291bcc75d14fbca62eb9b157e
silver0108 <kimsoeun0108@gmail.com>
2024-06-02 18:13:35 +0900

    feat: add UI for scheduling data and time

--- src/app/(schedule)/schedule/_components/schedule-form.tsx
+++ src/app/(schedule)/schedule/_components/schedule-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import PrimaryButton from '@/components/primary-button';
+import { cn } from '@/utils/cn';
 import { Checkbox, Input as HeadlessInput } from '@headlessui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ColorCircle = ({ color }: { color: string }) => {
   return (
@@ -20,6 +21,30 @@ const ScheduleForm = () => {
   const selectChange = () => {
     setSelect(!select);
   };
+
+  const [currentDate, setCurrentDate] = useState('');
+  const [currentTime, setCurrentTime] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  const [openCalendar, setOpenCalendar] = useState(false);
+  const [openTime, setOpenTime] = useState(false);
+
+  useEffect(() => {
+    const today = new Date();
+    const formattedDate = today.toISOString().split('T')[0].replace(/-/g, '.');
+    const fullTime = today.toTimeString().split(' ')[0];
+    const displayTime = fullTime.slice(0, 5);
+    setCurrentDate(formattedDate);
+    setCurrentTime(displayTime);
+
+    const endDate = new Date(today.getTime() + 60 * 60 * 1000);
+    const endFormattedDate = endDate.toISOString().split('T')[0].replace(/-/g, '.');
+    const endFullTime = endDate.toTimeString().split(' ')[0];
+    const endDisplayTime = endFullTime.slice(0, 5);
+    setEndDate(endFormattedDate);
+    setEndTime(endDisplayTime);
+  }, []);
 
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-between gap-10 px-4">
@@ -53,7 +78,78 @@ const ScheduleForm = () => {
         </div>
 
         {/* 날짜 및 시간 */}
-        <div className="h-[546px] bg-background">날짜 및 시간</div>
+        <div className="flex flex-col gap-4 bg-background">
+          <div className="flex gap-4">
+            <div className="flex flex-1 flex-col border-b-[0.75px] border-black">
+              <div className="label-small pb-2">시작일 *</div>
+              <div className="body-medium flex py-2">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenCalendar(!openCalendar);
+                    setOpenTime(false);
+                  }}
+                >
+                  <div
+                    className={cn(
+                      'data-[disabled]:text-[#111]/[.4]',
+                      openCalendar && 'text-primary',
+                    )}
+                  >
+                    {currentDate}
+                  </div>
+                </button>
+                <button
+                  className="flex-1"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTime(!openTime);
+                    setOpenCalendar(false);
+                  }}
+                >
+                  <div className="text-center">{currentTime}</div>
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-1 flex-col border-b-[0.75px] border-black">
+              <div className="label-small pb-2">종료일 *</div>
+              <div className="body-medium flex py-2">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenCalendar(!openCalendar);
+                    setOpenTime(false);
+                  }}
+                >
+                  <div
+                    className={cn(
+                      'data-[disabled]:text-[#111]/[.4]',
+                      openCalendar && 'text-primary',
+                    )}
+                  >
+                    {endDate}
+                  </div>
+                </button>
+                <button
+                  className="flex-1"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTime(!openTime);
+                    setOpenCalendar(false);
+                  }}
+                >
+                  <div className="text-center">{endTime}</div>
+                </button>
+              </div>
+            </div>
+          </div>
+          {openCalendar && <div>캘린더</div>}
+          {openTime && <div>시간</div>}
+          <div className="flex justify-end gap-2">
+            <div>매주 반복하기</div>
+            <div>토글</div>
+          </div>
+        </div>
 
         {/* 참석 인원 */}
         <div className="h-[112px] bg-gray">참석 인원</div>
