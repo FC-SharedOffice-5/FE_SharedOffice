7e4333ae29823e7ab6dbfafa84eaa7c71812444d
ovoxiix <ovoxiix@gmail.com>
2024-06-05 20:25:23 +0900

    feat: add a Calendar component

--- src/app/(schedule)/schedule/page.tsx
+++ src/app/(schedule)/schedule/page.tsx
@@ -1,15 +1,24 @@
-import ScheduleAddIcon from '@/assets/icons/schedule-add-icon';
-import Calendar from '@/components/calendar';
-import ScheduleList from './_components/schedule-list';
+'use client';
+
+import { useState } from 'react';
 import Header from '@/components/header';
+import Calendar from '@/components/calendar';
+import ScheduleAddIcon from '@/assets/icons/schedule-add-icon';
 
 export default function SchedulePage() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
   return (
     <main className="flex h-full flex-col">
       <Header title="일정" />
       <section className="flex-1">
-        <Calendar />
-        <ScheduleList />
+        <Calendar
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          currentMonth={currentMonth}
+          setCurrentMonth={setCurrentMonth}
+        />
       </section>
       <section className="sticky bottom-0">
         <div className="absolute -top-[135px] right-0 flex cursor-pointer">
--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,5 +1,81 @@
-const Calendar = () => {
-  return <div className="h-[208px] bg-gray text-white">주간 및 월간 캘린더</div>;
+import { Dispatch, SetStateAction } from 'react';
+import { addMonths, subMonths } from 'date-fns';
+import Monthly from './monthly';
+import Weekly from './weekly';
+import NextIcon from '@/assets/icons/next-icon';
+
+export type CalendarProps = {
+  title?: 'header' | 'arrow';
+  type?: 'monthly' | 'weekly';
+  selectedDate: Date;
+  setSelectedDate: Dispatch<SetStateAction<Date>>;
+  currentMonth: Date;
+  setCurrentMonth: Dispatch<SetStateAction<Date>>;
+};
+
+const Calendar = ({
+  title = 'header',
+  type = 'monthly',
+  selectedDate,
+  setSelectedDate,
+  currentMonth,
+  setCurrentMonth,
+}: CalendarProps) => {
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+  return (
+    <div className="flex flex-col gap-4">
+      {title === 'header' ? (
+        <div className="headline-medium text-black">
+          {currentMonth.getFullYear()}년 {currentMonth.getMonth() + 1}월
+        </div>
+      ) : (
+        <div className="title-medium flex justify-center gap-1 text-black">
+          <button
+            onClick={() => {
+              setCurrentMonth(subMonths(currentMonth, 1));
+            }}
+          >
+            <NextIcon rotate={true} />
+          </button>
+          {currentMonth.getFullYear()}년 {currentMonth.getMonth() + 1}월
+          <button
+            onClick={() => {
+              setCurrentMonth(addMonths(currentMonth, 1));
+            }}
+          >
+            <NextIcon />
+          </button>
+        </div>
+      )}
+      <div className="flex flex-col">
+        <div className="grid grid-cols-7">
+          {days.map((day) => (
+            <div
+              key={day.toString()}
+              className="label-medium text-center text-gray-600"
+            >
+              {day}
+            </div>
+          ))}
+        </div>
+        {type === 'monthly' ? (
+          <Monthly
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            currentMonth={currentMonth}
+          />
+        ) : (
+          <Weekly
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            currentMonth={currentMonth}
+            setCurrentMonth={setCurrentMonth}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
