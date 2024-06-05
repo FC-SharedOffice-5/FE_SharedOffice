ee7b6b0078784f206ef2bfb8f11e203a33d808ab
ovoxiix <ovoxiix@gmail.com>
2024-06-06 05:25:08 +0900

    feat: add a Timeline component

--- src/app/(schedule)/schedule/page.tsx
+++ src/app/(schedule)/schedule/page.tsx
@@ -3,11 +3,41 @@
 import { useState } from 'react';
 import Header from '@/components/header';
 import Calendar from '@/components/calendar';
+import Timeline from './_components/timeline';
 import ScheduleAddIcon from '@/assets/icons/schedule-add-icon';
 
 export default function SchedulePage() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const data = [
+    {
+      eventId: 1,
+      memberId: 1,
+      resId: 0,
+      eventColor: '#FFD2D5',
+      eventTitle: '제목',
+      eventStartDate: '2024-05-30T10:00:00',
+      eventEndDate: '2024-05-30T11:00:00',
+      eventLocation: '장소',
+      attendees: [
+        {
+          attendeesId: 2,
+          attendeesCode: 1,
+          memberId: 2,
+          attendeesCategory: 0,
+        },
+        {
+          attendeesId: 1,
+          attendeesCode: 1,
+          memberId: 1,
+          attendeesCategory: 0,
+        },
+      ],
+      createdAt: '2024-05-31T02:16:34.360712',
+      updatedAt: '2024-05-31T02:16:34.360712',
+    },
+  ];
 
   return (
     <main className="flex h-full flex-col">
@@ -19,6 +49,7 @@ export default function SchedulePage() {
           currentMonth={currentMonth}
           setCurrentMonth={setCurrentMonth}
         />
+        <Timeline data={data} />
       </section>
       <section className="sticky bottom-0">
         <div className="absolute -top-[135px] right-0 flex cursor-pointer">
