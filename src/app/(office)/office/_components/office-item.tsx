bd497659c4df6b985a5aaf4f8e0a769ee25f5066
ovoxiix <ovoxiix@gmail.com>
2024-06-11 03:05:04 +0900

    feat: add an office page

---
+++ src/app/(office)/office/_components/office-item.tsx
@@ -0,0 +1,50 @@
+import StarIcon from '@/assets/icons/star-icon';
+import PedestrianIcon from '@/assets/icons/pedestrian-icon';
+import CapacityIcon from '@/assets/icons/capacity-icon';
+import FacilityIcon from '@/assets/icons/facility-icon';
+
+type OfficeItemProps = {
+  data: {
+    officeId: number;
+    officeName: string;
+    officeAddr: string;
+    officeFloor: number;
+    officeTime: string;
+    officeCapacity: number;
+    officeStudio: number;
+    officeMeeting: number;
+    officeLatitude: string;
+    officeLongitude: string;
+    officeFacilities: string;
+    memberLike: boolean;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
+
+const OfficeItem = ({ data }: OfficeItemProps) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between border-b-[0.75px] border-black">
+        <span className="label-large text-black">Mile {data.officeName}</span>
+        <button>{data.memberLike ? <StarIcon /> : <StarIcon color="white" />}</button>
+      </div>
+      <div className="body-small flex flex-col gap-1 text-gray-700">
+        <div className="flex items-center gap-1">
+          <PedestrianIcon />
+          걸어서 5분
+        </div>
+        <div className="flex items-center gap-1">
+          <CapacityIcon />
+          수용인원 {data.officeCapacity}인
+        </div>
+        <div className="flex items-center gap-1">
+          <FacilityIcon />
+          {data.officeFacilities}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OfficeItem;
---
+++ src/assets/icons/pedestrian-icon.tsx
@@ -0,0 +1,24 @@
+const PedestrianIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+  >
+    <g clipPath="url(#pedestrian-icon_svg__a)">
+      <path
+        fill="#555755"
+        d="M12.923 6.02c1.015 0 1.846-.832 1.846-1.847s-.83-1.846-1.846-1.846c-1.015 0-1.846.83-1.846 1.846 0 1.015.83 1.846 1.846 1.846M9.508 9.157 6.923 22.173h1.939l1.661-7.385 1.938 1.847v5.538h1.847V15.25l-1.939-1.846.554-2.77A6.76 6.76 0 0 0 18 12.943v-1.846c-1.754 0-3.23-.923-3.97-2.215l-.922-1.477a1.98 1.98 0 0 0-2.447-.776L6 8.604v4.338h1.846V9.804z"
+      />
+    </g>
+    <defs>
+      <clipPath id="pedestrian-icon_svg__a">
+        <path
+          fill="#fff"
+          d="M0 0h24v24H0z"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default PedestrianIcon;
